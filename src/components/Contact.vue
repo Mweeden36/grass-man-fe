583f35d0ffe35297650c6aa112eb188ce01996ae
Mike Weeden <weeden.mike@gmail.com>
2018-06-15 21:31:58 -0400

    Starting a little work on contact page

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -1,17 +1,42 @@
 <template>
   <div class="contact bot-pad">
     <PageBanner pageName="Contact"/>
-    <h2>Contact</h2>
+    <h2>Get in Touch</h2>
+    <div class="contact-type">
+      <PhoneIcon/>
+      <h3>Phone</h3>
+      <p>{{phoneNumber}}</p>
+    </div>
+    <div class="contact-type">
+      <MailIcon/>
+      <h3>Email</h3>
+      <p>{{emailAddress}}</p>
+    </div>
+    <div class="contact-type">
+      <UsersIcon/>
+      <h3>Social</h3>
+      <p>Facebook: {{facebookLink}}</p>
+    </div>
   </div>
 </template>
 
 <script>
 import PageBanner from '@/components/PageBanner';
+import { UsersIcon, MailIcon, PhoneIcon } from 'vue-feather-icons';
+import config from '../assets/config.json';
 
 export default {
   name: 'Contact',
   components: {
     PageBanner,
+    PhoneIcon,
+    UsersIcon,
+    MailIcon,
+  },
+  data() {
+    return {
+      ...config,
+    };
   },
 };
 </script>
