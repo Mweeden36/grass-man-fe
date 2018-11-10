da905bffb7cb81d846702d591b48c94e4a2e06dd
Mike W <weeden.mike@gmail.com>
2018-11-10 11:29:15 -0500

    Converting Home page to use iView ui components.

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,27 +1,43 @@
 <template>
   <div class="hello bot-pad">
-    <div class="jumbotron">
-      <img src="@/assets/main-image.jpg">
-      <div class="hero">
-        <h1>No Job too Big or too Small</h1>
-        <h2>We Serve Better and Faster</h2>
-        <router-link to="/services" class="button get-estimate">Get a Free Estimate</router-link>
-      </div>
-    </div>
-    <div class="about-wrapper">
-      <h2>Welcome!</h2>
+    <Carousel
+      dots="none"
+      arrow="never"
+      class="jumbotron"
+      height="300px">
+      <CarouselItem>
+        <div class="hero">
+          <h1>No Job too Big or too Small</h1>
+          <h2>We Serve Better and Faster</h2>
+          <router-link to="/services" class="button get-estimate">
+            <Button>
+              Get a Free Estimate
+            </Button>
+          </router-link>
+        </div>
+      </CarouselItem>
+    </Carousel>
+    <Card class="about-wrapper">
+      <h2 slot="title">Welcome!</h2>
       <div class="about-container">
         <p>{{blurb}}</p>
       </div>
-    </div>
+    </Card>
   </div>
 </template>
 
 <script>
+import { Button, Card, Carousel, CarouselItem } from 'iview';
 import { blurb } from '../assets/config.json';
 
 export default {
   name: 'HelloWorld',
+  components: {
+    Button,
+    Card,
+    Carousel,
+    CarouselItem,
+  },
   data() {
     return {
       blurb,
@@ -39,74 +55,40 @@ export default {
     .jumbotron {
       overflow: hidden;
       width: 100%;
-      img {
-        height: auto;
-        width: 100%;
-        filter: brightness(0.7);
-        box-shadow: 2px 1px 2px $green-brown;
-      }
-      .hero {
-        //TODO: Do this better. example:
-        // item/grow-landscaping-and-gardening-wordpress-theme/full_screen_preview/20600417
+      box-shadow: 2px 1px 2px $green-brown;
+      color: #FFF;
+      &::before {
+        content: "";
         position: absolute;
-        top: 40%;
-        padding-left: 20px;
-        @include tablet {
-          top: 25%;
-        }
-        h1 {
-          color: white;
-          margin: 0px;
-          font-weight: 700;
-
-          @include phone {
-            font-size: 1.2em;
-          }
-          @include narrow-phone {
-            display: none;
-          }
-        }
-        h2 {
-          margin: 0px;
-          color: $light-blue;
-          font-weight: 500;
-
-          @include phone {
-            font-size: 1.0em;
-          }
-          @include narrow-phone {
-            display: none;
-          }
-        }
-        .button {
-          display: inline-block;
-          text-decoration: none;
-          color: white;
-          border-radius: 20px;
-          padding: 10px 15px;
-          border: 2px solid white;
-          @include desktop {
-            margin: 20px 0px 0px;
-          }
+        background-size: cover;
+        background-image: url("/static/img/main-image.jpg");
+        background-position-y: bottom;
+        filter: brightness(0.7);
+        top: 0;
+        left: 0;
+        height: 100%;
+        width: 100%;
+      }
+      .ivu-carousel-item {
+        display: flex;
+        align-items: center;
+        .hero {
+          padding: 0px 10px;
         }
       }
     }
     .about-wrapper {
+      margin: 10px auto 0px;
       width: 60%;
+      color: $blue;
       @include tablet {
         width: 80%;
       }
-      margin: 0px auto;
       h2 {
-        color: $blue;
         margin-bottom: 5px;
-        padding: 0px 20px;
       }
-      .about-container {
-        padding: 1px 20px;
-        p {
-          color: $blue;
-        }
+      > * {
+        padding: 0px 20px;
       }
     }
   }
