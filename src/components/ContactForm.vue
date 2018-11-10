b7a8709cac75b1a3589eaab60e29e7d6b6e45dcd
Mike W <weeden.mike@gmail.com>
2018-11-10 15:09:38 -0500

    Updating the Contact form to use iviewui components.

--- src/components/ContactForm.vue
+++ src/components/ContactForm.vue
@@ -1,116 +1,108 @@
 <template>
-  <div class="tight-padding">
-    <form id="contact-form" @submit="validateForm">
-      <h1>Contact Us</h1>
-      <p class="contact-blurb">{{contactBlurb}}</p>
-      <p v-if="errors.length" class="error-container">
-        <span>Please correct the following error(s):</span>
-        <ul class="error-list">
-          <li v-for="error in errors" v-bind:key="error">{{error}}</li>
-        </ul>
-      </p>
-      <div class="form-field name">
-        <label for="name-input" class="required">Name</label>
-        <input v-model="name"
-          id="name-input"
-          v-bind:class="{ error: this.nameError }"
-          @blur="validateName"
-          placeholder="Galileo Humpkins">
-        <p v-if="nameError" class="error-text">{{nameError}}</p>
-      </div>
-      <div class="form-field name">
-        <label for="email-input" class="required">Email Address</label>
-        <input v-model="email"
-          id="email-input"
-          v-bind:class="{ error: this.emailError }"
-          @blur="validateEmail"
-          placeholder="someone@example.com">
-        <p v-if="emailError" class="error-text">{{emailError}}</p>
-      </div>
-      <div class="form-field phone">
-        <label for="phone-input">Phone Number</label>
-        <input v-model="phone" id="phone-input" placeholder="(123) 555-1234">
-      </div>
-      <div class="form-field message">
-        <label for="message-input" class="required">Message</label>
-        <textarea
-          v-model="message" id="message-input"
-          placeholder="A message to The Grass Man!"
-          v-bind:class="{ error: this.messageError }"
-          @blur="validateMessage">
-        </textarea>
-        <p v-if="messageError" class="error-text">{{messageError}}</p>
-      </div>
-      <div class="form-field submit-container">
-        <button>Submit</button>
-      </div>
-    </form>
-  </div>
+<div class="tight-padding contact-form-card__wrap">
+  <Card class="contact-form-card">
+    <h1 slot="title">Contact Us</h1>
+    <h3 slot="title" class="contact-blurb">{{contactBlurb}}</h3>
+    <Form id="contact-form" :model="formValidate" :rules="rules" >
+      <FormItem label="Name" prop="name">
+        <Input v-model="formValidate.name"
+          :autofocus="true"
+          placeholder="Galileo Humpkins" />
+      </FormItem>
+      <FormItem label="E-Mail" prop="email">
+        <Input v-model="formValidate.email"
+          placeholder="someone@example.com" />
+      </FormItem>
+      <FormItem label="City" prop="city">
+        <Input v-model="formValidate.city"
+          placeholder="Rock Tavern" />
+      </FormItem>
+      <FormItem label="Phone Number" prop="phone">
+        <Input v-model="formValidate.phone" id="phone-input" placeholder="(123) 555-1234" />
+      </FormItem>
+      <FormItem label="Message" prop="message">
+        <Input
+          type="textarea"
+          :autosize="true"
+          :spellcheck="true"
+          v-model="formValidate.message"
+          placeholder="A message to The Grass Man!"/>
+      </FormItem>
+      <FormItem>
+        <Button type="primary" @click="handleSubmit('formValidate')">Submit</button>
+      </FormItem>
+    </Form>
+  </Card>
+</div>
 </template>
 
 <script>
+import { Button, Card, Form, FormItem, Input } from 'iview';
 import config from '../assets/config.json';
-
-const nameRegex = new RegExp(/^[^0-9]{2,}$/ui);
-// eslint-disable-next-line
-const emailRegex = new RegExp(/^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i);
 
 export default {
   name: 'ContactForm',
+  components: {
+    Button,
+    Card,
+    Form,
+    FormItem,
+    Input,
+  },
   data() {
     return {
-      errors: [],
-      name: null,
-      email: null,
-      phone: null,
-      message: null,
-      nameError: false,
-      emailError: false,
-      messageError: false,
+      formValidate: {
+        name: '',
+        email: '',
+        phone: '',
+        city: '',
+        message: '',
+      },
+      rules: {
+        name: [
+          {
+            required: true,
+            message: 'Please enter your name.',
+            trigger: 'blur',
+          },
+        ],
+        email: [
+          {
+            required: true,
+            message: 'Please enter your email address.',
+            trigger: 'blur',
+          },
+          {
+            type: 'email',
+            message: 'Please provide a valid email address.',
+            trigger: 'blur',
+          },
+        ],
+        city: [
+          {
+            required: true,
+            message: 'Please enter your city.',
+            trigger: 'blur',
+          },
+        ],
+        phone: [
+          { required: false },
+        ],
+        message: [
+          { required: false },
+        ],
+      },
       ...config,
     };
   },
   methods: {
-    validateName() {
-      if (!this.name || !this.name.match(nameRegex)) {
-        this.nameError = 'Please enter a valid name.';
-      } else {
-        this.nameError = false;
-      }
-      return this.nameError;
-    },
-    validateEmail() {
-      if (!this.email || !this.email.match(emailRegex)) {
-        this.emailError = 'Please enter a vaild email address.';
-      } else {
-        this.emailError = false;
-      }
-      return this.emailError;
-    },
-    validateMessage() {
-      if (!this.message) {
-        this.messageError = 'A message is required.';
-      } else {
-        this.messageError = false;
-      }
-      return this.messageError;
-    },
-    validateForm(e) {
-      e.preventDefault();
-      this.errors = [];
-      this.messageError = this.validateMessage();
-      this.emailError = this.validateEmail();
-      this.nameError = this.validateName();
-      const errors = [this.nameError, this.emailError, this.messageError];
-      if (!errors.filter(Boolean).length) {
-        return this.submitForm();
-      }
-      return {
-        error: errors,
-      };
-    },
-    submitForm() {
-      // console.log('TODO: Call API.');
+    handleSubmit() {
+      this.$refs[name].validate((valid) => {
+        if (valid) {
+          // console.log('TODO: Call API.');
+          this.$Message.success('Success!');
+        }
+      });
     },
   },
 };
@@ -118,74 +110,24 @@ export default {
 
 <style lang="scss" scoped>
 @import './src/styles/_variables.scss';
-$font-size: 1.1em;
+@import './src/styles/_mixins.scss';
 
-#contact-form {
+.contact-form-card__wrap {
   width: 75%;
   margin: 0 auto;
-  background-color: $light-blue;
-  color: $blue;
   margin-top: 25px;
   padding: 25px;
-  .error-list {
-    color: $yellow;
-    list-style-type: none;
+  @include tablet {
+    width: 100%;
   }
-  h1 {
-    margin: 0px 0px 5px;
-  }
-  .contact-blurb {
-    padding-bottom: 5px;
-  }
-  .form-field {
-    .error-text {
-      color: #cc0033;
-      font-size: 1.0em;
+  .contact-form-card {
+    background-color: $light-blue;
+    color: $blue;
+    .contact-blurb {
+      padding: 15px 0px;
     }
-    label, input, textarea {
-      display: block;
-      width: 100%;
-      &.error {
-        background-color: #fce4e4;
-        border: 1px solid #cc0033;
-        outline: none;
-      }
-    }
-    label {
-      font-size: $font-size;
-      line-height: 1.2em;
-      &.required:after {
-        content: "*";
-        color: $yellow;
-      }
-      margin-top: 15px;
-    }
-    input {
-      height: $font-size;
-      font-size: $font-size;
-    }
-    textarea {
-      height: 75px;
-      font-family: 'Avenir', Helvetica, Arial, sans-serif;
-      font-size: $font-size;
-    }
-  }
-  .submit-container {
-    text-align: right;
-    padding-top: 20px;
-    button {
-      height: 35px;
-      background: none;
-      outline: none;
-      color: $blue;
-      border: 2px solid $blue;
-      border-radius: 20px;
-      opacity: 0.8;
-      font-weight: 700;
-      cursor: pointer;
-      &:hover {
-        opacity: 1;
-      }
+    .ivu-form-item {
+      margin-bottom: 15px;
     }
   }
 }
